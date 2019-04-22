8fcba6cfdb018fbbc98760feab81fd98f437b547
Philipe Schneider <philipeschneider@gmail.com>
2019-04-22 09:32:01 -0300

    Define stream name of streams to workshop

--- src/main/java/kinesis/data/stream/Main.java
+++ src/main/java/kinesis/data/stream/Main.java
@@ -5,25 +5,27 @@ import kinesis.data.stream.consumer.sdk.ConsumerSdk;
 import kinesis.data.stream.producer.kpl.ProducerKpl;
 import kinesis.data.stream.producer.sdk.ProducerSdk;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     public static void main(String args[]) throws ExecutionException, InterruptedException {
+
         AwsConfiguration awsConfiguration = new AwsConfiguration("", "", "");
 
         ProducerSdk producerSdk = new ProducerSdk(awsConfiguration);
-        producerSdk.sendDataToStream("");
+        producerSdk.sendDataToStream("WORKSHOP_SDK");
 
         ProducerKpl producerKpl = new ProducerKpl(awsConfiguration);
-        producerKpl.sendDataAsync(null, "", "");
-        producerKpl.sendDataSync(null ,"", "");
-        producerKpl.sendDataBarebones(null, "", "");
+        producerKpl.sendDataAsync(Arrays.asList("DATA ASYNC"), "WORKSHOP_LIB", "WORKSHOP_LIB");
+        producerKpl.sendDataSync(Arrays.asList("DATA SYNC") ,"WORKSHOP_LIB", "WORKSHOP_LIB");
+        producerKpl.sendDataBarebones(Arrays.asList("DATA BAREBONE"), "WORKSHOP_LIB", "WORKSHOP_LIB");
 
         ConsumerSdk consumerSdk = new ConsumerSdk(awsConfiguration);
-        consumerSdk.getDataFromStream("");
+        consumerSdk.getDataFromStream("WORKSHOP_SDK");
 
-        ConsumerKcl consumerKcl = new ConsumerKcl(awsConfiguration, "", "");
+        ConsumerKcl consumerKcl = new ConsumerKcl(awsConfiguration, "WORKSHOP_LIB", "WORKSHOP_LIB");
         consumerKcl.run();
 
     }
